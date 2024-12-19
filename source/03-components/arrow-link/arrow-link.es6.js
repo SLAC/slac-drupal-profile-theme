24bb8248ccc41929e2d36e3e6a0e0738f1e0a9db
Brian Tofte-Schumacher <201665+briantschu@users.noreply.github.com>
2024-12-19 18:00:49 -0500

    If a last word has already been found, use that

--- source/03-components/arrow-link/arrow-link.es6.js
+++ source/03-components/arrow-link/arrow-link.es6.js
@@ -14,14 +14,25 @@ Drupal.behaviors.arrowLink = {
         return;
       }
 
-      // Get the deepest nested Text Node of the last child element in the
-      // link. This ensures we're accounting for markup within the <a> tag.
-      let lastTextChild = link.lastChild;
-      while (lastTextChild) {
-        if (lastTextChild.nodeType === Node.TEXT_NODE) {
-          break;
+      // Check if this element already has a child span with a *.__word class.
+      let lastTextChild = null;
+      const existingWordSpan = link.querySelector('[class$="__word"]');
+      if (existingWordSpan) {
+        // Move the last word text content out of the existing *.__word span, then remove the span.
+        lastTextChild = existingWordSpan.firstChild;
+        existingWordSpan.parentElement.appendChild(lastTextChild);
+        existingWordSpan.remove();
+      }
+      else {
+        // Get the deepest nested Text Node of the last child element in the
+        // link. This ensures we're accounting for markup within the <a> tag.
+        lastTextChild = link.lastChild;
+        while (lastTextChild) {
+          if (lastTextChild.nodeType === Node.TEXT_NODE) {
+            break;
+          }
+          lastTextChild = lastTextChild.lastChild;
         }
-        lastTextChild = lastTextChild.lastChild;
       }
 
       if (lastTextChild) {
--- source/03-components/external-link/external-link.es6.js
+++ source/03-components/external-link/external-link.es6.js
@@ -68,15 +68,27 @@ Drupal.behaviors.externalLinks = {
 
     externalLinks.forEach(el => {
       if (el.hasAttribute('href') && !el.querySelector('img')) {
-        // Get the deepest nested Text Node of the last child element in the
-        // link. This ensures we're accounting for markup within the <a> tag.
-        let lastTextChild = el.lastChild;
-        while (lastTextChild) {
-          if (lastTextChild.nodeType === Node.TEXT_NODE) {
-            break;
-          }
-          lastTextChild = lastTextChild.lastChild;
+        // Check if this element already has a child span with a *.__word class.
+        let lastTextChild = null;
+        const existingWordSpan = el.querySelector('[class$="__word"]');
+        if (existingWordSpan) {
+          // Move the last word text content out of the existing *.__word span, then remove the span.
+          lastTextChild = existingWordSpan.firstChild;
+          existingWordSpan.parentElement.appendChild(lastTextChild);
+          existingWordSpan.remove();
         }
+        else {
+         // Get the deepest nested Text Node of the last child element in the
+         // link. This ensures we're accounting for markup within the <a> tag.
+         lastTextChild = el.lastChild;
+         while (lastTextChild) {
+           if (lastTextChild.nodeType === Node.TEXT_NODE) {
+             break;
+           }
+           lastTextChild = lastTextChild.lastChild;
+         }
+        }
+
         if (lastTextChild) {
           const text = lastTextChild.parentElement.innerHTML;
           const textArray = text.trim().split(' ');
